6ed4bdbd6fc1c66d80e4c023993501b6cd1eccbb
hw2cb <dim5.dim5.960@mail.ru>
2021-06-29 16:39:42 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import {AppBar, Container, Toolbar, IconButton, Typography, Box} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) =>({
+  root:
+  {
+    flexGrow:1
+  },
+  menuButton:
+  {
+    marginRight:theme.spacing(1)
+  },
+  title:
+  {
+    flexGrow: 1
+  }
+}))
+function App() {
+  const classes = useStyles();
+  return (
+    <AppBar position = "fixed">
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label = "menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6"className={classes.title}>Мой сайт на React</Typography>
+          <Box mr={3}>
+            <Button color="inherit" variant="outlined">Log In</Button>
+          </Box>
+          <Button color="secondary" variant = "contained">Sign Up</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default App;
