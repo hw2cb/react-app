8c5d3251a0b3b8f8b515cad80c2961656f41364d
hw2cb <dim5.dim5.960@mail.ru>
2021-06-30 18:06:09 +0400

    start dev header

---
+++ src/components/header/head.jsx
@@ -0,0 +1,24 @@
+function Head()
+{
+    return 
+    <header>
+        <div className="content">
+            <div className="logo">
+                <div className="image">
+                    <img src="/image/logo.svg"></img>
+                </div>
+                <div className="title">
+                Саратовский государственный университет имени Гагарина Ю. А.
+                </div>
+            </div>
+            <div className="contact">
+
+            </div>
+            <div className="info">
+
+            </div>
+        </div>
+    </header>
+    ;
+}
+export default Head;
