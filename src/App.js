70080cb9ce99a89c05f8f141e601855c11655c6f
hw2cb <dim5.dim5.960@mail.ru>
2021-07-01 16:00:00 +0400

    add banner

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Header from './components/header/Header';
 import Logo from'./components/LogoNav/LogoNavigation';
 import Banner from './components/Banner/Banner';
-
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div>
         <Header />
         <Logo />
         <Banner />
+        <Footer/>
     </div>
   )
 }
