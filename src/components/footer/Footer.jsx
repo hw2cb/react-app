51daff0ae4c63f386cd3cc8a75404f8eb6582805
Makimaa <max_x9119@mail.ru>
2021-07-06 15:40:00 +0400

    made validation and change footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import map from "../image/map.svg";
-import fbIcon from "../image/facebook.svg";
-import vkIcon from "../image/vk.svg";
-import instaIcon from "../image/inst.svg";
+import fbIcon from "../image/facebook.png";
+import vkIcon from "../image/vk.png";
+import youtubeIcon from "../image/youtube.png";
+import instaIcon from "../image/inst.png";
 import StyleCss from "./footer.module.css";
 
 const Footer = () => {
+    /*Валидация*/
+    const [name, setName] = useState('');
+    const [nameDirty, setNameDirty] = useState(false);
+    const [nameError, setNameError] = useState('Имя не может быть пустым!');
+
+    const [email, setEmail] = useState('');
+    const [emailDirty, setEmailDirty] = useState(false);
+    const [emailError, setEmailError] = useState('Почтовый адрес не может быть пустым!');
+
+    const [phone, setPhone] = useState('');
+    const [phoneDirty, setPhoneDirty] = useState(false);
+    const [phoneError, setPhoneError] = useState('');
+
+    const [message, setMessage] = useState('');
+    const [messageDirty, setMessageDirty] = useState(false);
+    const [messageError, setMessageError] = useState('Ничего не введено...');
+
+    const [formValid, setFormValid] = useState(false);
+
+    useEffect(() => {
+        if (nameError || emailError) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [nameError, emailError]);
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value);
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Электронная почта некорректна...');
+        } else {
+            setEmailError('');
+        }
+    }
+
+    const phoneHandler = (e) => {
+        setPhone(e.target.value);
+        const re = /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g;
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setPhoneError('Неправильно введён номер телефона...');
+        } else {
+            setPhoneError('');
+        }
+    }
+
+    const messageHandler = (e) => {
+        setMessage(e.target.value);
+        if (!e.target.value) {
+            setMessageError('Имя не может быть пустым!');
+        } else {
+            setMessageError('');
+        }
+    }
+
+    const nameHandler = (e) => {
+        setName(e.target.value);
+        if (e.target.value.length > 30) {
+            setNameError('Ограничение по символам. Не больше 30.');
+            if (!e.target.value) {
+                setNameError('Имя не может быть пустым!');
+            }
+        } else {
+            setNameError('');
+        }
+    }
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break;
+            case 'name':
+                setNameDirty(true)
+                break;
+            case 'phone':
+                setPhoneDirty(true)
+                break;
+            case 'message':
+                setMessageDirty(true)
+                break;
+        }
+    };
+
     return (
         <footer>
             <div className = {StyleCss.info}>
@@ -14,27 +100,49 @@ const Footer = () => {
                     <img src = {map} />
                 </div>
                 <div className = {StyleCss.inputs}>  
-                    <input className = {StyleCss.name} type = "Имя" placeholder = "*Имя"/>
-                    <input className = {StyleCss.phone} type = "Телефон" placeholder = "Телефон"/>
-                    <input className = {StyleCss.email} type = "E-mail" placeholder = "*E-mail"/>
-                    <input className = {StyleCss.message} type = "Сообщение" placeholder = "Сообщение..."/>
+                    {(nameDirty && nameError) && <div style = {{color: '#DB538C'}}>{nameError}</div>}
+                    <input onChange = {e => nameHandler(e)} value = {name} onBlur = {e => blurHandler(e)} className = {StyleCss.name} name = "name" type = "Имя" placeholder = "*Имя" maxLength = "30"/>
+                    {(phoneDirty && phoneError) && <div style = {{color: '#DB538C'}}>{phoneError}</div>}
+                    <input onChange = {e => phoneHandler(e)} value = {phone} onBlur = {e => blurHandler(e)} className = {StyleCss.phone} name = "phone" type = "Телефон" placeholder = "Телефон" maxLength = "12"/>
+                    {(emailDirty && emailError) && <div style = {{color: '#DB538C'}}>{emailError}</div>}
+                    <input onChange = {e => emailHandler(e)} value = {email} onBlur = {e => blurHandler(e)} className = {StyleCss.email} name = "email" type = "E-mail" placeholder = "*E-mail"/>
+                    {(messageDirty && messageError) && <div style = {{color: '#DB538C'}}>{messageError}</div>}
+                    <input className = {StyleCss.message} onChange = {e => messageHandler(e)} value = {message} onBlur = {e => blurHandler(e)} name = "message" type = "Сообщение" placeholder = "Сообщение..." maxLength = "300"/>
+                    <div className = {StyleCss.btn}><button disabled = {!formValid} className = {StyleCss.send}><a href = "#"> Отправить </a></button></div>
                 </div> 
-                <div className = {StyleCss.btn}><button className = {StyleCss.send}><a href = "#"> Отправить </a></button></div>
             </div>
             <div className = {StyleCss.other}>
                 <div className = {StyleCss.otherInfo}>
-                    <div className = {StyleCss.aboutOfUs}><a href = "#"> О нас </a></div>
-                    <div className = {StyleCss.applicant}><a href = "#"> Абитуриенту </a></div>
-                    <div className = {StyleCss.courses}><a href = "#"> Курсы </a></div>
-                    <div className = {StyleCss.reviews}><a href = "#"> Отзывы </a></div>
-                    <div className = {StyleCss.preparation}><a href = "#"> Подготовка к ЕГЭ </a></div>
-                    <div className = {StyleCss.contacts}><a href = "#"> Контакты </a></div>
+                    <div className = {StyleCss.item}>
+                        <div className = {StyleCss.aboutOfUs}><a href = "#"> О нас </a></div>
+                        <div className = {StyleCss.courses}><a href = "#"> Курсы </a></div>
+                        <div className = {StyleCss.preparation}><a href = "#"> Подготовка к ЕГЭ </a></div>
+                    </div>
+                    <div className = {StyleCss.item}>
+                        <div className = {StyleCss.applicant}><a href = "#"> Абитуриенту </a></div>
+                        <div className = {StyleCss.reviews}><a href = "#"> Отзывы </a></div>
+                        <div className = {StyleCss.contacts}><a href = "#"> Контакты </a></div>
+                    </div>
+                    <div className = {StyleCss.icons}>
+                        <div className = {StyleCss.icon}>
+                            <a href = "#"><img src = {vkIcon} /></a>
+                        </div>
+                        <div className = {StyleCss.icon}>
+                            <a href = "#"><img src = {instaIcon} /></a>
+                        </div>
+                        <div className = {StyleCss.icon}>
+                            <a href = "#"><img src = {youtubeIcon} /></a>
+                        </div>
+                        <div className = {StyleCss.icon}>
+                            <a href = "#"><img src = {fbIcon} /></a>
+                        </div>
+                    </div>
                 </div>
-                <div className = {StyleCss.material}>
-                    <div> Детская компьютерная школа </div>
-                    <div> ул. Политехническая, д. 77. офис 417 </div>
-                    <div> +7 (8452) 99-87-34 </div>
-                </div>
+            </div>
+            <div className = {StyleCss.material}>
+                <div> Детская компьютерная школа </div>
+                <div> ул. Политехническая, д. 77. офис 417 </div>
+                <div> +7 (8452) 99-87-34 </div>
             </div>
             <div className = {StyleCss.ccs}> © 2000-2021 Детская компьютерная школа </div>
         </footer>
